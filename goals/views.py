e3d4ce5d9f0bdf1e42066443215f78fce515db01
Verevkina Elena <verevkinaee@gmail.com>
2022-05-10 17:02:29 +0300

    add goals app

---
+++ goals/views.py
@@ -0,0 +1,44 @@
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+from goals.models import GoalCategory
+from goals.serializers import GoalCreateSerializer, GoalCategorySerializer
+
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [IsAuthenticated]
+    serializer_class = GoalCreateSerializer
+
+
+class GoalCategoryListView(ListAPIView):
+    model = GoalCategory
+    permission_classes = [IsAuthenticated]
+    serializer_class = GoalCategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        OrderingFilter,
+        SearchFilter,
+    ]
+    ordering_fields = ["title", "created"]
+    ordering = ["title"]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+
+class GoalCategoryView(RetrieveUpdateDestroyAPIView):
+    model = GoalCategory
+    serializer_class = GoalCategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
+        return instance
+
