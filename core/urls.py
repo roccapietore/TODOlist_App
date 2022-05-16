75dca30191947d5b734803827017a31873f4c38e
Verevkina Elena <verevkinaee@gmail.com>
2022-05-16 17:04:23 +0300

    deploy

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from core.views import SignUpView, LoginView, EditProfileView, UpdatePasswordView
 
 urlpatterns = [
     path('signup', SignUpView.as_view()),
-    path('login', LoginView.as_view()),
+   # path('login', LoginView.as_view()),
     path('profile', EditProfileView.as_view()),
     path('update_password', UpdatePasswordView.as_view()),
+    path('login', csrf_exempt(LoginView.as_view())),
 ]
