5120d5957394e7e7e47c5c5e4101a42a0ccf1a0e
Verevkina Elena <verevkinaee@gmail.com>
2022-05-17 16:42:38 +0300

    add changes in categories views & permissions.py

--- goals/permissions.py
+++ goals/permissions.py
@@ -14,3 +14,41 @@ class BoardPermissions(permissions.BasePermission):
             filters['role'] = BoardParticipant.Role.owner
         return BoardParticipant.objects.filter(**filters).exists()
 
+
+class CategoryPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user:
+            return False
+        if not request.user.is_authenticated:
+            return False
+
+        filters: dict = {'user': request.user, 'board': obj.board}
+        if request.method not in permissions.SAFE_METHODS:
+            filters['role__in'] = [BoardParticipant.Role.owner, BoardParticipant.Role.writer]
+        return BoardParticipant.objects.filter(**filters).exists()
+
+
+class GoalPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user:
+            return False
+        if not request.user.is_authenticated:
+            return False
+
+        filters: dict = {'user': request.user, 'board': obj.category.board}
+        if request.method not in permissions.SAFE_METHODS:
+            filters['role__in'] = [BoardParticipant.Role.owner, BoardParticipant.Role.writer]
+        return BoardParticipant.objects.filter(**filters).exists()
+
+
+class CommentPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user:
+            return False
+        if not request.user.is_authenticated:
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
--- goals/views/categories.py
+++ goals/views/categories.py
@@ -1,44 +1,50 @@
+from django.db import transaction
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
-from goals.models import GoalCategory
+from django_filters.rest_framework import DjangoFilterBackend
+from goals.models import GoalCategory, Goal
+from goals.permissions import GoalPermissions
 from goals.serializers import CategoryCreateSerializer, GoalCategorySerializer
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
-    permission_classes = [IsAuthenticated]
+    permission_classes = [GoalPermissions]
     serializer_class = CategoryCreateSerializer
 
 
 class GoalCategoryListView(ListAPIView):
     model = GoalCategory
-    permission_classes = [IsAuthenticated]
+    permission_classes = [GoalPermissions]
     serializer_class = GoalCategorySerializer
     pagination_class = LimitOffsetPagination
     filter_backends = [
+        DjangoFilterBackend,
         OrderingFilter,
         SearchFilter,
     ]
+    filter_fields = ["board"]
     ordering_fields = ["title", "created"]
     ordering = ["title"]
     search_fields = ["title"]
 
     def get_queryset(self):
-        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+        return GoalCategory.objects.filter(board__participants__user=self.request.user, is_deleted=False)
 
 
 class GoalCategoryView(RetrieveUpdateDestroyAPIView):
     model = GoalCategory
     serializer_class = GoalCategorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [GoalPermissions]
 
     def get_queryset(self):
-        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+        return GoalCategory.objects.filter(board__participants__user=self.request.user, is_deleted=False)
 
     def perform_destroy(self, instance):
-        instance.is_deleted = True
-        instance.save()
+        with transaction.atomic():
+            instance.is_deleted = True
+            instance.goals.update(status=Goal.Status.archived)
+            instance.save()
         return instance
 
