b2798a7c66afe997f75256c35ffb871dfd34dce0
Verevkina Elena <verevkinaee@gmail.com>
2022-05-17 16:44:53 +0300

    add changes in permissions.py

--- goals/views/categories.py
+++ goals/views/categories.py
@@ -4,19 +4,19 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDe
 from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from goals.models import GoalCategory, Goal
-from goals.permissions import GoalPermissions
+from goals.permissions import CategoryPermissions
 from goals.serializers import CategoryCreateSerializer, GoalCategorySerializer
 
 
 class GoalCategoryCreateView(CreateAPIView):
     model = GoalCategory
-    permission_classes = [GoalPermissions]
+    permission_classes = [CategoryPermissions]
     serializer_class = CategoryCreateSerializer
 
 
 class GoalCategoryListView(ListAPIView):
     model = GoalCategory
-    permission_classes = [GoalPermissions]
+    permission_classes = [CategoryPermissions]
     serializer_class = GoalCategorySerializer
     pagination_class = LimitOffsetPagination
     filter_backends = [
@@ -36,7 +36,7 @@ class GoalCategoryListView(ListAPIView):
 class GoalCategoryView(RetrieveUpdateDestroyAPIView):
     model = GoalCategory
     serializer_class = GoalCategorySerializer
-    permission_classes = [GoalPermissions]
+    permission_classes = [CategoryPermissions]
 
     def get_queryset(self):
         return GoalCategory.objects.filter(board__participants__user=self.request.user, is_deleted=False)
