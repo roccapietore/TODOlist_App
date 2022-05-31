60f9fc565183a7e9909302b6a15e20c17914bf9c
Verevkina Elena <verevkinaee@gmail.com>
2022-05-31 10:53:24 +0300

    add changes in test_boards.py

--- goals/tests/test_boards.py
+++ goals/tests/test_boards.py
@@ -6,31 +6,29 @@ from goals.models import Board
 from goals.serializers import BoardCreateSerializer
 
 
-class TestSetUp(APITestCase):
+class TestBoard(APITestCase):
     def setUp(self):
-        self.user_data = User.objects.create(
+        self.user = User.objects.create(
             username='user',
             password='po324ure11'
         )
 
     def test_auth_req(self):
         url = reverse('board_create')
-        response = self.client.post(url, {'title': 'bord'})
+        response = self.client.post(url, {'title': 'board'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_create(self):
+    def test_create_board(self):
         url = reverse('board_create')
-        self.client.force_login(self.user_data)
+        self.client.force_login(self.user)
         response = self.client.post(url, {'title': 'board'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         resp_json = response.json()
         self.assertEqual(resp_json['title'], 'board')
         self.assertEqual(resp_json['is_deleted'], False)
 
-
-
     def tearDown(self):
-        self.user_data.delete()
+        self.user.delete()
 
 
 
