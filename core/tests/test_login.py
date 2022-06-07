5982121cb621a8900f07ed0a7f2e1fb24d0c67bb
Verevkina Elena <verevkinaee@gmail.com>
2022-06-07 14:52:23 +0300

    add changes in tests

--- core/tests/test_login.py
+++ core/tests/test_login.py
@@ -7,46 +7,43 @@ from goals.models import Board, GoalCategory, BoardParticipant, Goal, GoalCommen
 
 class TestUserLogin(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(username='Solder', password='po324ure11')
-        self.user_2 = User.objects.create(username='Poly', password='sjnfUu30kl0')
-        self.board = Board.objects.create(title='Title1')
-        self.category = GoalCategory.objects.create(title='Category', user=self.user, board=self.board)
-        self.new_participant = BoardParticipant.objects.create(board=self.board, user=self.user, role=1)
-        self.goal = Goal.objects.create(title='Goal_title', category=self.category,
-                                        status=1, priority=1, user=self.user)
-        self.comment = GoalComment.objects.create(text='Comment_text', goal=self.goal, user=self.user)
-        self.participant = BoardParticipant.objects.create(board=self.board, user=self.user_2, role=1)
+        self.user = User.objects.create(username='Solder')
+        self._user_password = 'po324ure11'
+        self.user.set_password(self._user_password)
+        self.user.save(update_fields=['password'])
 
     def tearDown(self):
         self.client.logout()
-        GoalComment.objects.all().delete()
-        Goal.objects.all().delete()
-        GoalCategory.objects.all().delete()
-        BoardParticipant.objects.all().delete()
-        Board.objects.all().delete()
         User.objects.all().delete()
 
     def test_auth_req_user(self):
         response = self.client.patch(reverse(viewname='profile'), {})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    # def test_user_login(self):
-    #     response = self.client.post('login', {"username": self.user.username,
-    #                                           "password": self.user.password,
-    #                                           })
-    #     resp_json = response.json()
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(resp_json, {"username": 'User', "password": 'po324ure11'})
-    #
-    # def test_signup(self):
-    #     response = self.client.post('signup', {'username': "newuser",
-    #                                            "password": "passUUik39kl",
-    #                                            "password_repeat": "passUUik39kl",
-    #                                            })
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     resp_json = response.json()
-    #     self.assertEqual(resp_json["username"], 'newuser')
-    #     self.assertEqual(resp_json["password"], 'passUUik39kl')
+    def test_user_login(self):
+        response = self.client.post('login', {"username": self.user.username,
+                                              "password": self._user_password,
+                                              })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertDictEqual(
+            response.json(),
+            {
+                'id': self.user.id,
+                'username': self.user.username,
+                'first_name': self.user.first_name,
+                'last_name': self.user.last_name,
+                'email': self.user.email
+            })
+
+    def test_signup(self):
+        response = self.client.post('signup', {"username": self.user.username,
+                                               "password": self._user_password,
+                                               "password_repeat": self._user_password,
+                                               })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        resp_json = response.json()
+        self.assertEqual(resp_json["username"], 'Solder')
+
 
     # def test_get_user(self):
     #     self.client.force_login(self.user)
@@ -80,6 +77,3 @@ class TestUserLogin(APITestCase):
     #     self.assertEqual(response.status_code, status.HTTP_200_OK)
     #     resp_json = response.json()
     #     self.assertEqual(resp_json['new_password'], 'ne32nkeoo28')
-
-
-
